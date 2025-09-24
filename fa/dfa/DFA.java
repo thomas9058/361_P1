a51b0ce4e1bd25b47241f1c2eff72fb11d2afb8e
Dalton Bilau Goncalves <daltonbilaugonca@u.boisestate.edu>
2025-09-23 21:39:48 -0600

    Updated DFA

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -1,29 +1,43 @@
 
+import com.sun.net.httpserver.Authenticator;
+
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import fa.dfa.DFAInterface;
 
-public class DFA implements DFAInterface {
-
+public class DFA implements DFAInterface{
     
         private Set<State> state;
-        private String delta;
+        private Map<String, Map<String, Character>> delta;
         private Set<Character> sigma;
-        private Set<State> final;
+        private Set<State> finalState;
         private String initial;
 
     public DFA(){
         this.state = new HashSet<>();
-        this.delta = initial;
+        this.delta = new HashMap<>();
         this.sigma = new HashSet<>();
-        this.final = new HashSet<>();
+        this.finalState = new HashSet<>();
         this.initial = null;
     }
 
     @Override
     public boolean addTransition(String fromState, String toState, char onSymb) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        boolean success = false;
+        if(getState(fromState) != null && getState(toState) != null
+            && sigma.contains(onSymb)){
+                delta.putIfAbsent(fromState, new HashMap<>());
+                delta.get(fromState).put(toState, onSymb);
+                delta.put(fromState, innerMap);
+            success= true;
+        }
+        return success;
+
+        //References
+        //https://howtodoinjava.com/java/collections/hashmap/java-nested-map/#:~:text=Creating%20Nested%20Map%20using%20Map,()%3B%20addressMap.
     }
 
     @Override
@@ -33,26 +47,38 @@ public class DFA implements DFAInterface {
 
     @Override
     public boolean addState(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addState'");
+        boolean success = false;
+        if(getState(name) == null){
+            State newState = new State(name);
+            state.add(newState);
+            success = true;
+        }
+        return success;
     }
 
     @Override
     public boolean setFinal(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setFinal'");
+        Boolean success = false;
+        if(getState(name) != null){
+            finalState.add(name);
+            success = true;
+        }
+        return success;
     }
 
     @Override
     public boolean setStart(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setStart'");
+        Boolean success = false;
+        if(getState(name) != null){
+            initial = name;
+            success = true;
+        }
+        return success;
     }
 
     @Override
     public void addSigma(char symbol) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addSigma'");
+        sigma.add(symbol);
     }
 
     @Override
